0d204ab3624355c794544b2d066e2998364ae547
Nan211704 <nandanijangir03@gmail.com>
2026-05-18 02:40:02 +0530

    Reapply "docker implementation"
    This reverts commit d4e15c2aa3c4718d7d5f66d32062c238da17fa71.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "my-cross-stock-icons.s3.ap-south-1.amazonaws.com",
+        pathname: "/icons/**",
+      },
+      {
+        protocol: "https",
+        hostname: "*.s3.*.amazonaws.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
